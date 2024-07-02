b48fb6cddeea129c731722875fa3ed09c6b07953
Brinda Kaul <kaul.brinda@gmail.com>
2024-07-02 19:37:58 +0530

    HTML home page

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 socketio = SocketIO(app)
 
-@app.route("/", methods["POST", "GET"])
+@app.route("/", methods = ["POST", "GET"])
 def home():
     return render_template("home.html")
 
